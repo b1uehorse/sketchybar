6041d5a9543ab85d66cc35d27796e3d4dd5aca0e
b1uehorse <269769195+b1uehorse@users.noreply.github.com>
2026-04-16 22:06:39 +0400

    bluetooth: click native BT dropdown via ControlCenter accessibility

--- plugins/bluetooth.sh
+++ plugins/bluetooth.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-osascript -e 'tell application "System Events" to tell process "ControlCenter" to click menu bar item "Bluetooth" of menu bar 1' 2>/dev/null
+osascript -e 'tell application "System Events" to tell process "ControlCenter" to click (first menu bar item of menu bar 1 whose description is "Bluetooth")' 2>/dev/null
