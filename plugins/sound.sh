f1ac6dde41041e2af835c65b8d16c320e00648dc
b1uehorse <269769195+b1uehorse@users.noreply.github.com>
2026-04-16 22:08:50 +0400

    add wifi, sound, battery widgets

---
+++ plugins/sound.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+osascript -e 'tell application "System Events" to tell process "ControlCenter" to click (first menu bar item of menu bar 1 whose description is "Sound")' 2>/dev/null
